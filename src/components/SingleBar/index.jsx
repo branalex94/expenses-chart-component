ea013aa603aefb36417d5f724c03259ce20831c8
Brandon Aray <alex.aray94@gmail.com>
2022-07-18 14:08:44 -0500

    Added basic animation to price popup on bar hover

--- src/components/SingleBar/index.jsx
+++ src/components/SingleBar/index.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 const DAYS = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
 
 export default function Singlebar({ day, amount, containerHeight }) {
+  const [isHover, setIsHover] = useState(false);
   const cleanHeight = parseInt(containerHeight.replace("px", ""));
   const today = new Date().getDay();
+  const handleMouseOver = (e) => {
+    console.log("mouse over");
+    setIsHover(true);
+  };
+  const handleMouseOut = (e) => {
+    console.log("mouse out");
+    setIsHover(false);
+  };
   return (
     <div className="bar">
       <span className="bar__day">{day}</span>
@@ -11,7 +22,16 @@ export default function Singlebar({ day, amount, containerHeight }) {
         style={{
           height: `${(parseInt(amount) * cleanHeight) / 100}px`,
         }}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
       ></span>
+      <span
+        className={`bar__price-overlay ${
+          isHover ? "bar__price-overlay--active" : ""
+        }`}
+      >
+        ${amount}
+      </span>
     </div>
   );
 }
